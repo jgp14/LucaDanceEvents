f81c2064fa0c8a99b3f6300e0c7634039ee5254c
Ruben Grueso Vega <ruben.gve@gmail.com>
2024-03-14 10:14:22 +0100

    first commit

---
+++ src/main/java/com/lucatic/grupo2/app/EventsGrupo2Application.java
@@ -0,0 +1,13 @@
+package com.lucatic.grupo2.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsGrupo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsGrupo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucatic/grupo2/app/EventsGrupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucatic.grupo2.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsGrupo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
