7e4bb05aacf03745348153c2df55150c60bef82f
Daniel <dangimrod@gmail.com>
2024-03-14 11:25:13 +0100

    Controler save

---
+++ src/main/java/com/lucatic/grupo2/app/control/EventController.java
@@ -0,0 +1,38 @@
+package com.lucatic.grupo2.app.control;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("/event")
+class EventController {
+
+	@Autowired
+	private EventService eventService;
+
+	@Autowired
+	private EventAdapter eventAdapter;
+
+	@PostMapping
+	public ResponseEntity<?> save(@Valid @RequestBody EventRequest eventRequest) throws EventExistException{
+
+		try {
+			Event event= eventService.save(eventRequest);
+			URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(event.getId()
+					.toUri();
+			LOGGER.info("Event " + event.getName() + " with id " + event.getId() + " has been created");
+			return ResponseEntity.created(location).build();
+
+		} catch (EventExistException e) {
+            		LOGGER.warn("Error dando de alta el juego " + e.getMessage());
+            		throw e;
+        	}
+	}
+}
