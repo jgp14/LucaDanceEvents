1c5fe1faf61d5607f39d8e2e8d70bfcc188d42b9
Daniel <dangimrod@gmail.com>
2024-03-15 10:32:10 +0100

    Repositori y service comment, falta serviceimpl

--- src/main/java/com/lucatic/grupo2/app/repository/EventRepository.java
+++ src/main/java/com/lucatic/grupo2/app/repository/EventRepository.java
@@ -2,6 +2,11 @@ package com.lucatic.grupo2.app.repository;
 
 import com.lucatic.grupo2.app.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+/**
+ * Genera una interfaz que hereda metodos de accion en la base de datos, gestiona objetos tipo Event
+ * @author BlueDevTeam
+ * @version 1.0.0
+ * @since 15-03-2024
+ */
 public interface EventRepository extends JpaRepository<Event, Long> {
 }
--- src/main/java/com/lucatic/grupo2/app/repository/EventRoomRepository.java
+++ src/main/java/com/lucatic/grupo2/app/repository/EventRoomRepository.java
@@ -2,6 +2,11 @@ package com.lucatic.grupo2.app.repository;
 
 import com.lucatic.grupo2.app.model.EventRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+/**
+ * Genera una interfaz que hereda metodos de accion en la base de datos, gestiona objetos tipo EventRoom
+ * @author BlueDevTeam
+ * @version 1.0.0
+ * @since 15-03-2024
+ */
 public interface EventRoomRepository extends JpaRepository<EventRoom, Long> {
 }
