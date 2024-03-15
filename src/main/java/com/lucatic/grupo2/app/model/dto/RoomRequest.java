16c40fb217843f9c00bfd0e094a4a2f23bac5e2d
jgp14 <jaimegarciaparada@gmail.com>
2024-03-15 12:40:04 +0100

    documentation dto

--- src/main/java/com/lucatic/grupo2/app/model/dto/RoomRequest.java
+++ src/main/java/com/lucatic/grupo2/app/model/dto/RoomRequest.java
@@ -3,16 +3,37 @@ package com.lucatic.grupo2.app.model.dto;
 import com.lucatic.grupo2.app.model.Room;
 import lombok.*;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 
+/**
+ * Clase modelo DTO RoomRequest para peticiones en relacion con salas. Posee los
+ * getters, setters, toString y constuctores generados por Lombok @Data.
+ * 
+ * @author BlueDevTeams
+ * @version v1.0.0
+ * @since 15-03-2024
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RoomRequest {
 
-    private Room room;
-    private String date;
-    private String initTime;
-    private String endTime;
+	/**
+	 * Atrubuto Clase Room que ya incluye los atributos de la sala
+	 */
+	private Room room;
+
+	/**
+	 * Atributo string con la fecha de la sala parseada.
+	 */
+	private String date;
+
+	/**
+	 * Atributo string con la fecha inicial parseada del evento en la sala
+	 */
+	private String initTime;
+
+	/**
+	 * Atributo string con la fecha final parseada del evento en la sala
+	 */
+	private String endTime;
 }
