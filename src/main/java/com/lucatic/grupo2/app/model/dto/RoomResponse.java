e4e81db20500b0ca49ba44347107b2917e7d3530
rafa <rafaelgarciaespada@gmail.com>
2024-03-18 16:37:43 +0100

    comentarios

--- src/main/java/com/lucatic/grupo2/app/model/dto/RoomResponse.java
+++ src/main/java/com/lucatic/grupo2/app/model/dto/RoomResponse.java
@@ -10,12 +10,45 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RoomResponse {
 
+	/**
+	 * Atributo string fecha del evento
+	 */
     private String date;
+    
+    /**
+	 * Atributo string hora de inicio del evento
+	 */
     private String initTime;
+    
+    /**
+	 * Atributo string hora de fin del evento
+	 */
+    
     private String endTime;
+    
+    /**
+	 * Atributo string nombre de la sala
+	 */
+    
     public String name;
+    
+    /**
+	 * Atributo string ciudad de la sala
+	 */
     private String city;
+    
+    /**
+	 * Atributo string dirección de la sala
+	 */
     private String address;
+    
+    /**
+	 * Atributo string tipo de sala
+	 */
     private String roomType;
+    
+    /**
+	 * Atributo string capacidad de la sala
+	 */
     private Integer capacity;
 }
